335bff9522e4a923ce889abea3921925c7e76962
FernandoGabrielSalguero <149271548+FernandoGabrielSalguero@users.noreply.github.com>
2026-02-04 11:28:24 -0300

    main

--- views/admin/admin_regalosColegio.php
+++ views/admin/admin_regalosColegio.php
@@ -118,14 +118,14 @@ $formatDate = function ($value) {
                     <li onclick="location.href='admin_usuarios.php'">
                         <span class="material-icons" style="color: #5b21b6;">people</span><span class="link-text">Usuarios</span>
                     </li>
-                    <li onclick="location.href='admin_regalosColegio.php'">
-                        <span class="material-icons" style="color: #5b21b6;">card_giftcard</span><span class="link-text">Regalos</span>
-                    </li>
                     <li onclick="location.href='admin_cuyoPlacas.php'">
                         <span class="material-icons" style="color: #5b21b6;">factory</span><span class="link-text">Cuyo Placas</span>
                     </li>
                     <li onclick="location.href='admin_logs.php'">
                         <span class="material-icons" style="color: #5b21b6;">history</span><span class="link-text">Logs</span>
+                    </li>
+                    <li onclick="location.href='admin_regalosColegio.php'">
+                        <span class="material-icons" style="color: #5b21b6;">card_giftcard</span><span class="link-text">Regalos</span>
                     </li>
                     <li onclick="location.href='../../../logout.php'">
                         <span class="material-icons" style="color: red;">logout</span><span class="link-text">Salir</span>
